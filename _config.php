6e6b9feae06d5a79b01a37d9c5387e1c58eb80ad
Nicolaas Francken <github@sunnysideup.co.nz>
2011-08-15 03:53:10 +0000

    adding hints for paypal config

--- _config.php
+++ _config.php
@@ -1,1 +1,10 @@
 <?php
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START payment_paypal MODULE ----------------===================
+//PayPalExpressCheckoutPayment::set_config_details($username,$password,$signature,$testmode = true);
+//PayPalExpressCheckoutPayment::set_live_config($username,$password,$signature);
+//PayPalExpressCheckoutPayment::set_test_config($username,$password,$signature);
+//PayPalExpressCheckoutPayment::set_custom_settings(array());
+//===================---------------- END payment_paypal MODULE ----------------===================
