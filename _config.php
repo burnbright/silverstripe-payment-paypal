86df46189a9e44f1ac52fe266a7ec288c3b1576e
Nicolaas Francken <github@sunnysideup.co.nz>
2011-10-09 13:29:33 +0000

    adding little help in _config

--- _config.php
+++ _config.php
@@ -3,6 +3,7 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START payment_paypal MODULE ----------------===================
+// see API Access under https://www.paypal.com/webapps/customerprofile/summary.view
 //PayPalExpressCheckoutPayment::set_config_details($username,$password,$signature,$testmode = true);
 //PayPalExpressCheckoutPayment::set_live_config($username,$password,$signature);
 //PayPalExpressCheckoutPayment::set_test_config($username,$password,$signature);
